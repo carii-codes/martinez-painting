79e22cc82ec77c8bc252aa32bd99d107af16b277
Carina Cardozo <cardozo1747@gmail.com>
2023-02-13 23:11:27 -0600

    navbar size

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "16px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "35px 10px";
+    document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "30px";
   }
 }
